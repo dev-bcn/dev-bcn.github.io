04f8c02626bb6a69a9e0a5c7a41d1f30872db59d
Mourad Mounim <mourad.mounim@plainconcepts.com>
2022-02-12 10:10:41 +0100

    Add transition to navigation

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -3,22 +3,20 @@ import { FC } from 'react';
 import styled from 'styled-components';
 import { COLOR_PINK } from '../../styles/colors';
 
-const initial = { x: '-100vx' };
-const animate = { x: 0 };
-const transition = { delay: 2, duration: 3 };
 
 const StyledNavigation = styled(motion.nav)`
   background: ${COLOR_PINK};
   position: absolute;
   top: 0;
   bottom: 0;
+  width: 100vw;
 `;
 const Navigation: FC = () => {
   return (
     <StyledNavigation
-      initial={initial}
-      animate={animate}
-      transition={transition}
+      initial = {{ width: 0 }}
+      animate = {{ width: "100vw" }}
+      transition = {{ duration: .5 }}
       className='navigation-container'
     >
       <p>aqui un div que ocupara todo</p>
