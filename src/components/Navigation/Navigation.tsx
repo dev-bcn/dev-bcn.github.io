497f8181ec1f6c94b119b711611fbdeb3958a030
mourad5000 <mouradmounim7@gmail.com>
2022-02-15 19:52:39 +0100

    Change some styles

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,14 @@
-import { AnimatePresence, motion } from "framer-motion";
-import { FC } from "react";
-import styled from "styled-components";
-import { COLOR_PINK, COLOR_YELLOW } from "../../styles/colors";
-import CloseIcon from "../../assets/images/CloseIcon.svg";
-import { NavLink } from "react-router-dom";
-import { navigationItems } from "./NavigationData";
-import TicketIcon from "../../assets/images/TicketIcon.svg";
-import { useWindowSize } from "react-use";
+import { AnimatePresence, motion } from 'framer-motion';
+import { FC } from 'react';
+import styled from 'styled-components';
+import { COLOR_PINK, COLOR_YELLOW } from '../../styles/colors';
+import CloseIcon from '../../assets/images/CloseIcon.svg';
+import { NavLink } from 'react-router-dom';
+import { navigationItems } from './NavigationData';
+import TicketsImage from '../../assets/images/TicketsImage.svg';
+import { useWindowSize } from 'react-use';
 import { MOBILE_BREAKPOINT } from '../../constants/BreakPoints';
+import { StyledMenuIcon } from '../../views/Home/Home';
 
 const StyledNavigationContainer = styled(motion.div)`
   position: absolute;
@@ -16,7 +17,6 @@ const StyledNavigationContainer = styled(motion.div)`
   display: flex;
   z-index: 1;
   transform: translate3d(0px, 0px, 0px);
-  /* visibility: visible; */
   transition: transform 0.5s ease 0s;
 `;
 
@@ -36,44 +36,28 @@ const StyledClipPath = styled(motion.div)`
   background: ${COLOR_PINK};
 `;
 
-const StyledCloseMenuImage = styled(motion.img)`
-  position: absolute;
-  top: 2rem;
-  right: 3rem;
-  width: 30px;
-  height: 30px;
-  cursor: pointer;
-`;
-
 const StyledLink = styled(NavLink)`
+  padding: 0.5rem;
   color: ${COLOR_YELLOW};
   text-decoration: none;
-  padding: 0.5rem;
-  opacity: 1;
-
+  cursor: pointer;
   &.${(props) => props.activeClassName} {
-      &:before {
-        content: "/   ";
-      }
+    &:before {
+      content: '/   ';
+    }
   }
 `;
 
-const HighlightLinkContainer = styled.div`
-  background: ${COLOR_YELLOW};
-  display: flex;
-  width: fit-content;
-  margin: 0.5rem;
-`;
-
 const StyledNavLinkHighlighted = styled(motion.a)`
-  color: ${COLOR_PINK};
   text-decoration: none;
-  padding: 0.25rem 1.5rem;
+  margin: 0.5rem;
   display: flex;
+  cursor: pointer;
 `;
 
 const StyledNavLinkHighlightedImage = styled.img`
-  padding-right: .75rem;
+  width: 9rem;
+  height: auto;
 `;
 
 type TNavigationProps = {
@@ -82,19 +66,21 @@ type TNavigationProps = {
 };
 
 const Navigation: FC<TNavigationProps> = ({ isOpened, setMenu }) => {
-  const { width, height } = useWindowSize();
+  const { width } = useWindowSize();
 
   return (
     <AnimatePresence>
       {isOpened && (
         <StyledNavigationContainer
-          transition={{ duration: .5, ease: "linear"}}
+          transition={{ duration: 0.5, ease: 'easeIn' }}
           initial={{ width: 0 }}
-          animate={ width > MOBILE_BREAKPOINT ? {width: "140vw"}:{width: "100vw"}}
+          animate={
+            width > MOBILE_BREAKPOINT ? { width: '140vw' } : { width: '100vw' }
+          }
           exit={{ width: 0 }}
         >
           <StyledNavigation>
-            <StyledCloseMenuImage
+            <StyledMenuIcon
               src={CloseIcon}
               onClick={setMenu}
               whileTap={{ scale: 0.8 }}
@@ -104,20 +90,19 @@ const Navigation: FC<TNavigationProps> = ({ isOpened, setMenu }) => {
                 key={item.id}
                 to={item.link}
                 onClick={setMenu}
-                activeClassName="isActive"
+                activeClassName='isActive'
               >
                 {item.id}
               </StyledLink>
             ))}
-            <HighlightLinkContainer>
-              <StyledNavLinkHighlighted href="https://www.google.es/" target="_blank">
-              <StyledNavLinkHighlightedImage src={TicketIcon}/>
-                TICKETS
-              </StyledNavLinkHighlighted>
-            </HighlightLinkContainer>
+            <StyledNavLinkHighlighted
+              href='https://www.google.es/'
+              target='_blank'
+            >
+              <StyledNavLinkHighlightedImage src={TicketsImage} />
+            </StyledNavLinkHighlighted>
           </StyledNavigation>
-          {/* esto se quita con el width en mobile */}
-          <StyledClipPath />
+          {width > MOBILE_BREAKPOINT && <StyledClipPath />}
         </StyledNavigationContainer>
       )}
     </AnimatePresence>
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,27 +1,26 @@
-import { FC, useState } from "react";
-import { useWindowSize } from "react-use";
-import { motion } from "framer-motion";
-import styled from "styled-components";
-import HomeImage from "../../assets/images/HomeImage.jpg";
-import Navigation from "../../components/Navigation/Navigation";
-import HamburgerIcon from "../../assets/images/HamburgerIcon.svg";
+import { FC, useState } from 'react';
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+import HomeImage from '../../assets/images/HomeImage.jpg';
+import Navigation from '../../components/Navigation/Navigation';
+import HamburgerIcon from '../../assets/images/HamburgerIcon.svg';
 
-const StyledHamburger = styled(motion.img)`
+export const StyledMenuIcon = styled(motion.img)`
   position: absolute;
   top: 2rem;
   right: 3rem;
-  width: 30px;
-  height: 30px;
-  cursor: poiner;
+  width: 2.5rem;
+  height: 2.5rem;
+  cursor: pointer;
 `;
 
-  const StyleHomeContainer = styled.div`
-    background-image: url(${HomeImage});
-    background-size: cover;
-    background-position: center center;
-    background-repeat: no-repeat;
-    height: 100vh;
-  `;
+const StyleHomeContainer = styled.div`
+  background-image: url(${HomeImage});
+  background-size: cover;
+  background-position: center center;
+  background-repeat: no-repeat;
+  height: 100vh;
+`;
 
 const Home: FC = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -34,7 +33,7 @@ const Home: FC = () => {
     <>
       <StyleHomeContainer />
       <Navigation isOpened={isOpen} setMenu={handleSetMenu} />
-      <StyledHamburger
+      <StyledMenuIcon
         onClick={handleSetMenu}
         src={HamburgerIcon}
         whileTap={{ scale: 0.8 }}
