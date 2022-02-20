7b5c174504f48c6914eda478a79654335e4b5833
Mourad Mounim <mourad.mounim@plainconcepts.com>
2022-02-20 20:48:13 +0100

    Add faqs section

--- src/views/Faqs/Faqs.tsx
+++ src/views/Faqs/Faqs.tsx
@@ -1,29 +1,49 @@
-import { FC } from 'react';
-import { motion } from 'framer-motion';
-import styled from 'styled-components';
-import { COLOR_PINK } from '../../styles/colors';
-import { MOBILE_BREAKPOINT } from '../../constants/BreakPoints';
-import { faqsData } from './FaqsData';
-import FaqCard from './components/FaqsCard';
+import { FC } from "react";
+import { motion } from "framer-motion";
+import styled from "styled-components";
+import { COLOR_PINK } from "../../styles/colors";
+import { faqsData } from "./FaqsData";
+import FaqCard from "./components/FaqsCard";
 
 const StyledFaqSection = styled(motion.section)`
   background: ${COLOR_PINK};
-  padding: 3rem 5rem;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  @media (max-width: ${MOBILE_BREAKPOINT}) {
+  padding: 3rem 2rem;
+  @media (min-width: 650px) {
+    padding: 3rem 5rem;
   }
+`;
+
+const StyledWaveContainer = styled.div`
+  background: ${COLOR_PINK};
+  overflow-y: hidden;
+  height: 5rem;
 `;
 
 const Faqs: FC = () => {
   return (
-    <StyledFaqSection>
-      {faqsData.map((faq) => (
-        <FaqCard faq={faq} />
-      ))}
-    </StyledFaqSection>
+    <>
+      <StyledFaqSection>
+        {faqsData.map((faq, index) => (
+          <FaqCard faq={faq} index={index} key={index} />
+        ))}
+      </StyledFaqSection>
+      <StyledWaveContainer>
+        <svg
+          viewBox="0 0 500 150"
+          preserveAspectRatio="none"
+          style={{ height: "100%", width: "100%" }}
+        >
+          <path
+            d="M0.00,49.98 C335.50,45.89 358.63,106.09 501.41,81.42 L500.00,150.00 L0.00,150.00 Z"
+            style={{ stroke: "none", fill: "#06d6a0" }}
+          ></path>
+        </svg>
+      </StyledWaveContainer>
+    </>
   );
 };
 
--- src/views/Faqs/components/FaqsCard.tsx
+++ src/views/Faqs/components/FaqsCard.tsx
@@ -1,42 +1,83 @@
-import { FC, Fragment } from 'react';
-import styled from 'styled-components';
-import { motion } from 'framer-motion';
-import ReadMoreIcon from '../../../assets/images/ReadMoreIcon.svg';
-import { Link } from 'react-router-dom';
-import FaqsImage3 from '../../../assets/images/FaqsImage3.png';
+import { FC } from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import ReadMoreIcon from "../../../assets/images/ReadMoreIcon.svg";
+import { Link } from "react-router-dom";
+import { COLOR_WHITE } from "../../../styles/colors";
 
 type FaqCardType = {
   faq: {
-    imageUrl: string;
     question: string;
     answer: string;
   };
+  index: number;
 };
 
-const StyledFaqCard = styled(motion.div)`
+const StyledFaqCard = styled.div`
   display: flex;
   align-items: center;
+  flex-direction: column;
+  width: 90%;
+  margin-bottom: 2rem;
+  @media (min-width: 800px) {
+    flex-direction: ${(props) => (props.color ? props.color : "row")};
+    max-width: 900px;
+    margin-bottom: 1rem;
+  }
 `;
 
 const StyledFaqImage = styled(motion.img)`
   height: 10rem;
+  @media (min-width: 800px) {
+    height: auto;
+    width: 40%;
+  }
 `;
 
 const StyledFaqInfo = styled(motion.div)`
   display: flex;
   flex-direction: column;
-  justify-content: flex-start;
+  color: ${COLOR_WHITE};
+  @media (min-width: 800px) {
+    width: 40%;
+    align-items: flex-start;
+  }
 `;
 
-const FaqCard: FC<FaqCardType> = ({ faq }) => {
+const StyledFaqTitle = styled.h2`
+  padding-top: 1rem;
+  @media (min-width: 800px) {
+    text-align: start;
+    padding: 0 1rem;
+  }
+`;
+
+const StyledFaqText = styled.p`
+  padding: 0.5rem;
+  @media (min-width: 800px) {
+    text-align: start;
+  }
+`;
+
+const StyledFaqDetailButton = styled.img`
+  width: 6rem;
+  @media (min-width: 800px) {
+    padding: 0 .5rem;
+    width: 7rem;
+  }
+`;
+
+const FaqCard: FC<FaqCardType> = ({ faq, index }) => {
+  const isOdd = index % 2 === 0 ? true : false;
+
   return (
-    <StyledFaqCard>
-      <StyledFaqImage src={FaqsImage3} />
+    <StyledFaqCard color={isOdd ? "row" : "row-reverse"}>
+      <StyledFaqImage src={require(`../../../assets/images/FaqsImage${index}.png`)} />
       <StyledFaqInfo>
-        <h2>{faq.question}</h2>
-        <p>{faq.answer}</p>
-        <Link to='/'>
-          <img src={ReadMoreIcon} />
+        <StyledFaqTitle>{faq.question}</StyledFaqTitle>
+        <StyledFaqText>{faq.answer}</StyledFaqText>
+        <Link to="/">
+          <StyledFaqDetailButton src={ReadMoreIcon} />
         </Link>
       </StyledFaqInfo>
     </StyledFaqCard>
