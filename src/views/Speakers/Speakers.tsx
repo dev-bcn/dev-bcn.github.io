72ed88ea3e2e864e92eaa0fb53ff598cc54647bf
mourad5000 <mouradmounim7@gmail.com>
2022-02-23 17:48:25 +0100

    Create the speakers compo

--- src/views/Speakers/Speakers.tsx
+++ src/views/Speakers/Speakers.tsx
@@ -1,27 +1,67 @@
 import { FC } from 'react';
 import styled from 'styled-components';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Autoplay, Pagination } from 'swiper';
 import TitleSection from '../../components/SectionTitle/TitleSection';
 import { COLOR_GREEN, COLOR_WHITE } from '../../styles/colors';
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import './Speakers.scss';
+
 const StyledSpeakersContainer = styled.section`
   background-color: ${COLOR_GREEN};
+  position: relative;
 `;
 
 const Speakers: FC = () => {
+  const params = {
+    spaceBetween: 30,
+    slidesPerView: 3,
+    pagination: true,
+    Autoplay: true,
+  };
   return (
-    <StyledSpeakersContainer>
-      <TitleSection
-        title='SPEAKERS'
-        subtitle='Are you a technology company? Interested in meeting
+    <>
+      <StyledSpeakersContainer>
+        <TitleSection
+          title='SPEAKERS'
+          subtitle='Are you a technology company? Interested in meeting
         enthusiasts and geek people for technology? This is a
         priceless opportunity to participate in the first big Java
         and JVM conference in Spain, to pr mote your company
         and to support technological communities.'
-        color={COLOR_WHITE}
-      />
-      {/* https://codesandbox.io/s/bjvh8?file=/src/App.jsx
-      ejemplo de carousel */}
-    </StyledSpeakersContainer>
+          color={COLOR_WHITE}
+        />
+
+        <Swiper {...params}>
+          <SwiperSlide>
+            <img
+              src={require(`../../assets/images/FaqsImage1.png`)}
+              alt='hola'
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={require(`../../assets/images/FaqsImage1.png`)}
+              alt='hola'
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={require(`../../assets/images/FaqsImage1.png`)}
+              alt='hola'
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img
+              src={require(`../../assets/images/FaqsImage1.png`)}
+              alt='hola'
+            />
+          </SwiperSlide>
+        </Swiper>
+      </StyledSpeakersContainer>
+    </>
   );
 };
 
