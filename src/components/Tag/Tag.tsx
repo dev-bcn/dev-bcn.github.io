d86b1ac7e9b4e8f52b2f4af32822f98b3bf2c6bc
Anyul Rivas <anyulled@gmail.com>
2022-10-16 20:31:29 +0200

    feat: change data for 2023 edition
    - splitting styles from components for re-usability.

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -1,29 +1,10 @@
-import { FC } from 'react';
-import styled from 'styled-components';
-import {
-  Color
-} from '../../styles/colors';
+import {FC} from 'react';
+import {Color} from '../../styles/colors';
+import {StyledTagText, StyledTagWrapper} from "./Style.Tag";
 
 type TagProps = {
   text: string;
 };
-
-const StyledTagWrapper = styled.div<{ borderColor: string }>`
-  border: ${({ borderColor }) => {
-    return `1px solid ${borderColor}`;
-  }};
-  background: transparent;
-  border-radius: 5px;
-  margin: 0.25rem;
-`;
-
-const StyledTagText = styled.p<{ color: string }>`
-  padding: 0.25rem 0.5rem;
-  color: ${({ color }) => {
-    return color;
-  }};
-  font-size: 0.75rem;
-`;
 
 const colorTagByText = (text: string) => {
   let color;
