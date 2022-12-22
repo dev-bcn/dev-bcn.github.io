f8a4c1dbca3d3bf3f134f097aa3bd0080e8b77d2
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2022-12-22 17:54:39 +0100

    feat: speakers page
    * General list and detail page
    * changes in the breadcrumb
    * new routes defined

--- src/views/SpeakerDetail/SpeakerDetail.tsx
+++ src/views/SpeakerDetail/SpeakerDetail.tsx
@@ -1,125 +1,161 @@
-import {BIG_BREAKPOINT} from '../../constants/BreakPoints';
+import { BIG_BREAKPOINT } from "../../constants/BreakPoints";
 
-import {FC} from 'react';
-import {ISpeakerDetail} from './SpeakerDetailData';
-import MoreThanIcon from '../../assets/images/MoreThanBlueIcon.svg';
-import SlashesWhite from '../../assets/images/SlashesWhite.svg';
-import linkedinIcon from '../../assets/images/linkedinIcon.svg';
-import twitterIcon from '../../assets/images/twitterIcon.svg';
-import {useWindowSize} from 'react-use';
+import { FC } from "react";
+import MoreThanIcon from "../../assets/images/MoreThanBlueIcon.svg";
+import SlashesWhite from "../../assets/images/SlashesWhite.svg";
+import linkedinIcon from "../../assets/images/linkedinIcon.svg";
+import twitterIcon from "../../assets/images/twitterIcon.svg";
+import { useWindowSize } from "react-use";
 import {
-    downVariants,
-    leftVariants,
-    noVariants,
-    rightVariants,
-    StyledDetailsContainer,
-    StyledFlexCol,
-    StyledImageContainer,
-    StyledInfoContainer,
-    StyledLink,
-    StyledMoreThanIcon,
-    StyledMoreThanIconContainer,
-    StyledName,
-    StyledNameContainer,
-    StyledRightContainer,
-    StyledSlashes,
-    StyledSocialMediaContainer,
-    StyledSocialMediaIcon,
-    StyledSpeakerDescription,
-    StyledSpeakerDetailContainer,
-    StyledSpeakerImg,
-    StyledSpeakerImgBorder,
-    StyledSpeakerTitle
+  downVariants,
+  leftVariants,
+  noVariants,
+  rightVariants,
+  StyledDetailsContainer,
+  StyledFlexCol,
+  StyledImageContainer,
+  StyledInfoContainer,
+  StyledLink,
+  StyledMoreThanIcon,
+  StyledMoreThanIconContainer,
+  StyledName,
+  StyledNameContainer,
+  StyledRightContainer,
+  StyledSlashes,
+  StyledSocialMediaContainer,
+  StyledSocialMediaIcon,
+  StyledSpeakerDescription,
+  StyledSpeakerDetailContainer,
+  StyledSpeakerImg,
+  StyledSpeakerImgBorder,
+  StyledSpeakerTitle,
 } from "./Speaker.style";
-import {ROUTE_TALK_DETAIL} from "../../constants/routes";
-import {StyledTalkDescription} from "./SpeakerDetail.style";
+import { ROUTE_SPEAKERS, ROUTE_TALK_DETAIL } from "../../constants/routes";
+import { StyledTalkDescription } from "./SpeakerDetail.style";
+import { ISpeaker } from "../Speakers/Speakers";
+import { Link } from "react-router-dom";
+import { Color } from "../../styles/colors";
 
 interface ISpeakerDetailProps {
-    speaker: ISpeakerDetail;
+  speaker: ISpeaker;
 }
 
-const SpeakerDetail: FC<ISpeakerDetailProps> = ({speaker}) => {
-    const {width} = useWindowSize();
-    return (
-        <>
-            <StyledSpeakerDetailContainer
-                className='DetailsContainer'
-                initial='initial'
-                animate='animate'
-                variants={width < BIG_BREAKPOINT ? downVariants : noVariants}
+const SpeakerDetail: FC<ISpeakerDetailProps> = ({ speaker }) => {
+  const { width } = useWindowSize();
+  return (
+    <>
+      <StyledSpeakerDetailContainer
+        className="DetailsContainer"
+        initial="initial"
+        animate="animate"
+        variants={width < BIG_BREAKPOINT ? downVariants : noVariants}
+      >
+        <StyledDetailsContainer className="DetailsContainerInner">
+          {width > BIG_BREAKPOINT && (
+            <StyledImageContainer
+              initial="initial"
+              animate="animate"
+              variants={leftVariants}
             >
-                <StyledDetailsContainer className='DetailsContainerInner'>
-                    {width > BIG_BREAKPOINT && (
-                        <StyledImageContainer
-                            initial='initial'
-                            animate='animate'
-                            variants={leftVariants}
-                        >
-                            <StyledSpeakerImgBorder>
-                                <StyledSpeakerImg photo={speaker.photoUrl}/>
-                            </StyledSpeakerImgBorder>
-                            <StyledSocialMediaContainer>
-                                <StyledLink href={speaker.twitterUrl} target={'_blank'}>
-                                    <StyledSocialMediaIcon src={twitterIcon}/>
-                                </StyledLink>
-                                <StyledLink href={speaker.linkedinUrl} target={'_blank'}>
-                                    <StyledSocialMediaIcon src={linkedinIcon} noMargin/>
-                                </StyledLink>
-                            </StyledSocialMediaContainer>
-                        </StyledImageContainer>
+              <StyledSpeakerImgBorder>
+                <StyledSpeakerImg photo={speaker.speakerImage} />
+              </StyledSpeakerImgBorder>
+              <StyledSocialMediaContainer>
+                {speaker.twitterUrl && (
+                  <StyledLink href={speaker.twitterUrl.url} target={"_blank"}>
+                    <StyledSocialMediaIcon src={twitterIcon} />
+                  </StyledLink>
+                )}
+                {speaker.linkedInUrl && (
+                  <StyledLink href={speaker.linkedInUrl.url} target={"_blank"}>
+                    <StyledSocialMediaIcon src={linkedinIcon} noMargin />
+                  </StyledLink>
+                )}
+              </StyledSocialMediaContainer>
+            </StyledImageContainer>
+          )}
+          <StyledRightContainer>
+            <StyledNameContainer
+              className="DetailsTitle"
+              initial="initial"
+              animate="animate"
+              variants={width < BIG_BREAKPOINT ? noVariants : rightVariants}
+            >
+              <StyledName>{speaker.fullName}</StyledName>
+              {width < BIG_BREAKPOINT && (
+                <>
+                  <StyledSpeakerImgBorder>
+                    <StyledSpeakerImg photo={speaker.speakerImage} />
+                  </StyledSpeakerImgBorder>
+                  <StyledSocialMediaContainer>
+                    {speaker.twitterUrl && (
+                      <StyledLink
+                        href={speaker.twitterUrl.url}
+                        target={"_blank"}
+                      >
+                        <StyledSocialMediaIcon src={twitterIcon} />
+                      </StyledLink>
                     )}
-                    <StyledRightContainer>
-                        <StyledNameContainer
-                            className='DetailsTitle'
-                            initial='initial'
-                            animate='animate'
-                            variants={width < BIG_BREAKPOINT ? noVariants : rightVariants}
-                        >
-                            <StyledName>{speaker.name}</StyledName>
-                            {width < BIG_BREAKPOINT && (
-                                <>
-                                    <StyledSpeakerImgBorder>
-                                        <StyledSpeakerImg photo={speaker.photoUrl}/>
-                                    </StyledSpeakerImgBorder>
-                                    <StyledSocialMediaContainer>
-                                        <StyledLink href={speaker.twitterUrl} target={'_blank'}>
-                                            <StyledSocialMediaIcon src={twitterIcon}/>
-                                        </StyledLink>
-                                        <StyledLink href={speaker.linkedinUrl} target={'_blank'}>
-                                            <StyledSocialMediaIcon src={linkedinIcon} noMargin/>
-                                        </StyledLink>
-                                    </StyledSocialMediaContainer>
-                                </>
-                            )}
-                            <StyledSlashes src={SlashesWhite}/>
-                        </StyledNameContainer>
-                        <StyledInfoContainer
-                            initial='initial'
-                            animate='animate'
-                            variants={downVariants}
-                        >
-                            <StyledFlexCol>
-                                <StyledSpeakerTitle>{speaker.title1}</StyledSpeakerTitle>
-                                <StyledSpeakerDescription>
-                                    {speaker.description1}
-                                </StyledSpeakerDescription>
+                    {speaker.linkedInUrl && (
+                      <StyledLink
+                        href={speaker.linkedInUrl.url}
+                        target={"_blank"}
+                      >
+                        <StyledSocialMediaIcon src={linkedinIcon} noMargin />
+                      </StyledLink>
+                    )}
+                  </StyledSocialMediaContainer>
+                </>
+              )}
+              <StyledSlashes src={SlashesWhite} />
+            </StyledNameContainer>
+            <StyledInfoContainer
+              initial="initial"
+              animate="animate"
+              variants={downVariants}
+            >
+              <StyledFlexCol>
+                <StyledSpeakerTitle>{speaker.tagLine}</StyledSpeakerTitle>
+                <StyledSpeakerDescription>
+                  {speaker.bio}
+                </StyledSpeakerDescription>
 
-                                <StyledTalkDescription to={ROUTE_TALK_DETAIL + "grace-jansen"}>
-                                    <StyledSpeakerTitle>{speaker.title2}</StyledSpeakerTitle>
-                                    <StyledSpeakerDescription>
-                                        {speaker.description2}
-                                    </StyledSpeakerDescription>
-                                </StyledTalkDescription>
-                            </StyledFlexCol>
-                            <StyledMoreThanIconContainer>
-                                <StyledMoreThanIcon src={MoreThanIcon}/>
-                            </StyledMoreThanIconContainer>
-                        </StyledInfoContainer>
-                    </StyledRightContainer>
-                </StyledDetailsContainer>
-            </StyledSpeakerDetailContainer>
-        </>
-    );
+                {speaker.sessions && <h2>Sessions</h2>}
+                <ul>
+                  {speaker.sessions &&
+                    speaker.sessions.map((session, index) => (
+                      <li key={index}>
+                        <StyledTalkDescription
+                          to={ROUTE_TALK_DETAIL + session.id}
+                        >
+                          <StyledSpeakerTitle>
+                            {session.name}
+                          </StyledSpeakerTitle>
+                        </StyledTalkDescription>
+                      </li>
+                    ))}
+                </ul>
+
+                <Link
+                  to={ROUTE_SPEAKERS}
+                  style={{
+                    color: Color.MAGENTA,
+                    fontWeight: "bold",
+                    textDecoration: "none",
+                  }}
+                >
+                  🔙 Go back
+                </Link>
+              </StyledFlexCol>
+              <StyledMoreThanIconContainer>
+                <StyledMoreThanIcon src={MoreThanIcon} />
+              </StyledMoreThanIconContainer>
+            </StyledInfoContainer>
+          </StyledRightContainer>
+        </StyledDetailsContainer>
+      </StyledSpeakerDetailContainer>
+    </>
+  );
 };
 
 export default SpeakerDetail;
--- src/views/Speakers/components/SpeakersCard.tsx
+++ src/views/Speakers/components/SpeakersCard.tsx
@@ -1,77 +1,31 @@
 import { FC } from "react";
-import styled from "styled-components";
-import {
-  MOBILE_BREAKPOINT,
-  TABLET_BREAKPOINT,
-} from "../../../constants/BreakPoints";
-import { Color } from "../../../styles/colors";
 import { ISpeaker } from "../Speakers";
+import {
+  StyledImageAnimation,
+  StyledSpeakerCard,
+  StyledSpeakerImage,
+  StyledSpeakerImageContainer,
+  StyledSpeakerText,
+  StyledSpeakerTitle,
+} from "./SpeakerCard.Style";
+import { Link } from "react-router-dom";
+import { ROUTE_SPEAKER_DETAIL } from "../../../constants/routes";
 
 type SpeakersCardProps = {
   speaker: ISpeaker;
 };
 
-const StyledSpeakerCard = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  width: 10rem;
-  padding: 0 1rem 1rem 1rem;
-  @media (min-width: ${TABLET_BREAKPOINT}px) {
-    width: 12rem;
-  }
-  @media (min-width: ${MOBILE_BREAKPOINT}px) {
-    width: 15rem;
-  }
-`;
-
-const StyledSpeakerImageContainer = styled.div`
-  width: 100%;
-  height: auto;
-  position: relative;
-`;
-
-const StyledSpeakerImage = styled.img`
-  width: 100%;
-  height: auto;
-  display: block;
-`;
-
-const StyledImageAnimation = styled.div`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  width: 100%;
-  opacity: 0;
-  transition: 0.25s linear;
-  background-color: rgba(239, 71, 111, 0.5);
-
-  &:hover {
-    opacity: 1;
-  }
-`;
-
-const StyledSpeakerTitle = styled.h5`
-  color: ${Color.WHITE};
-`;
-
-const StyledSpeakerText = styled.p`
-  color: ${Color.WHITE};
-  text-align: left;
-`;
-
 export const SpeakerCard: FC<SpeakersCardProps> = ({ speaker }) => {
   return (
     <StyledSpeakerCard>
-      <StyledSpeakerImageContainer>
-        <StyledSpeakerImage src={speaker.speakerImage} />
-        <StyledImageAnimation />
-      </StyledSpeakerImageContainer>
-      <StyledSpeakerTitle>{speaker.title}</StyledSpeakerTitle>
-      <StyledSpeakerText>{speaker.text}</StyledSpeakerText>
+      <Link to={`${ROUTE_SPEAKER_DETAIL}/${speaker.id}`}>
+        <StyledSpeakerImageContainer>
+          <StyledSpeakerImage src={speaker.speakerImage} />
+          <StyledImageAnimation />
+        </StyledSpeakerImageContainer>
+        <StyledSpeakerTitle>{speaker.fullName}</StyledSpeakerTitle>
+        <StyledSpeakerText>{speaker.tagLine}</StyledSpeakerText>
+      </Link>
     </StyledSpeakerCard>
   );
 };
