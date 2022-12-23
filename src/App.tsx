2b9f80f23591972c5c7ee604d24c5d3c25f090ed
Anyul Rivas <anyulled@gmail.com>
2022-12-23 09:28:03 +0100

    feat: add cookie consent (#20)

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,8 @@ import { Diversity } from "./views/Diversity/Diversity";
 import { Travel } from "./views/Travel/Travel";
 import React from "react";
 import NotFoundError from "./components/NotFoundError/NotFoundError";
+import { CookieConsent } from "react-cookie-consent";
+import { Color } from "./styles/colors";
 
 const StyledAppWrapper = styled.div`
   position: relative;
@@ -64,6 +66,23 @@ const App = () => {
         <Route path={ROUTE_HOME_ALTERNATE} component={HomeWrapper} exact />
         <Route path="*" component={NotFoundError} />
       </Switch>
+      <CookieConsent
+        debug={true}
+        enableDeclineButton={true}
+        cookieName="DevBcnCookie"
+        style={{ backgroundColor: Color.DARK_BLUE }}
+        buttonStyle={{
+          backgroundColor: Color.LIGHT_BLUE,
+          color: Color.WHITE,
+          fontWeight: "bold",
+        }}
+        declineButtonStyle={{
+          fontWeight: "bold",
+          backgroundColor: Color.MAGENTA,
+        }}
+      >
+        This website uses cookies to enhance the user experience.
+      </CookieConsent>
       <Footer />
     </StyledAppWrapper>
   );
