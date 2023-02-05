6969d11b54bc035a2f37c582ad122399bf7ecd4b
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-02-05 12:50:19 +0100

    chore: remove console logs

--- src/index.tsx
+++ src/index.tsx
@@ -13,9 +13,6 @@ const tracking_ID =
   process.env.REACT_APP_GOOGLE_ANALYTICS_API_KEY || "G-0BG1LNPT11";
 ReactGa.initialize(tracking_ID);
 
-console.info(`app version: ${process.env.npm_package_version}`);
-console.info(process.env);
-
 Sentry.init({
   dsn: "https://fdfaeed3901d43a299b95c534e42a4c1@o194303.ingest.sentry.io/4504348314435584",
   integrations: [new BrowserTracing()],
