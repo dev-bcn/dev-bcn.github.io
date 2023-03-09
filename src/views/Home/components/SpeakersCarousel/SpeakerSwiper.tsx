db28128bed30461a456cc05aa2d22cd3ac4c486b
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-03-09 14:19:28 +0100

    fix: include approved speakers from Sessionize API

--- src/views/Home/components/SpeakersCarousel/SpeakerSwiper.tsx
+++ src/views/Home/components/SpeakersCarousel/SpeakerSwiper.tsx
@@ -2,12 +2,13 @@ import { FC } from "react";
 import { Autoplay, Parallax } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react";
 import styled from "styled-components";
-import { speakersData } from "../../../Speakers/SpeakersData";
 import { Color } from "../../../../styles/colors";
 import "swiper/swiper-bundle.min.css";
 import "./SpeakersCarousel.scss";
 import { Link } from "react-router-dom";
 import { ROUTE_SPEAKER_DETAIL } from "../../../../constants/routes";
+import { useFetchSpeakers } from "../../../Speakers/UseFetchSpeakers";
+import * as Sentry from "@sentry/react";
 
 const StyledSlideImage = styled.img`
   display: block;
@@ -34,58 +35,71 @@ const StyledSlideText = styled.p`
   color: white;
 `;
 const SpeakerSwiper: FC = () => {
+  const { isLoading, data, error } = useFetchSpeakers();
+
+  const swiperSpeakers = data?.sort(() => 0.5 - Math.random()).slice(0, 20);
+
+  if (error) {
+    Sentry.captureException(error);
+  }
+
   return (
-    <Swiper
-      autoplay={{
-        delay: 500,
-        disableOnInteraction: true,
-      }}
-      slidesPerView={1}
-      spaceBetween={10}
-      speed={3000}
-      parallax={true}
-      loop={true}
-      grabCursor={true}
-      breakpoints={{
-        340: {
-          width: 640,
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-        768: {
-          width: 768,
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-        1024: {
-          width: 1024,
-          slidesPerView: 4,
-          centeredSlides: true,
-          spaceBetween: 30,
-          autoHeight: true,
-        },
-      }}
-      centeredSlides={true}
-      modules={[Autoplay, Parallax]}
-      className="mySwiper"
-    >
-      {speakersData.map((speaker) => (
-        <SwiperSlide key={speaker.id}>
-          <Link
-            to={`${ROUTE_SPEAKER_DETAIL}/${speaker.id}`}
-            style={{ textDecoration: "none" }}
-          >
-            <StyledSlideImage
-              src={speaker.speakerImage}
-              alt={speaker.fullName}
-            />
-            <StyledSlideContain>
-              <StyledSlideText>{speaker.fullName}</StyledSlideText>
-            </StyledSlideContain>
-          </Link>
-        </SwiperSlide>
-      ))}
-    </Swiper>
+    <>
+      {isLoading && <p>Loading</p>}
+      {swiperSpeakers && (
+        <Swiper
+          autoplay={{
+            delay: 500,
+            disableOnInteraction: true,
+          }}
+          slidesPerView={1}
+          spaceBetween={10}
+          speed={3000}
+          parallax={true}
+          loop={true}
+          grabCursor={true}
+          breakpoints={{
+            340: {
+              width: 640,
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+            768: {
+              width: 768,
+              slidesPerView: 3,
+              spaceBetween: 10,
+            },
+            1024: {
+              width: 1024,
+              slidesPerView: 4,
+              centeredSlides: true,
+              spaceBetween: 30,
+              autoHeight: true,
+            },
+          }}
+          centeredSlides={true}
+          modules={[Autoplay, Parallax]}
+          className="mySwiper"
+        >
+          {swiperSpeakers.map((speaker) => (
+            <SwiperSlide key={speaker.id}>
+              <Link
+                to={`${ROUTE_SPEAKER_DETAIL}/${speaker.id}`}
+                style={{ textDecoration: "none" }}
+              >
+                <StyledSlideImage
+                  src={speaker.speakerImage}
+                  alt={speaker.fullName}
+                />
+                <StyledSlideContain>
+                  <StyledSlideText>{speaker.fullName}</StyledSlideText>
+                </StyledSlideContain>
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      )}
+    </>
   );
 };
 
