6b0b28627d53044cb6a4e527c2831509de1c0b64
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-05-12 13:00:26 +0200

    fix: improve layout on Job Offers page

--- src/views/JobOffers/CompanyOffers.tsx
+++ src/views/JobOffers/CompanyOffers.tsx
@@ -1,129 +1,57 @@
 import React from "react";
-import styled from "styled-components";
-import { Company } from "./JobsData";
+import { CompanyProps } from "./JobsData";
 import { Color } from "../../styles/colors";
 import LinkedinIcon from "../../components/Icons/Linkedin";
 import TwitterIcon from "../../components/Icons/Twitter";
+import {
+  CompanyContainer,
+  CompanyDescription,
+  CompanyLogo,
+  CompanyName,
+  OfferLink,
+  OfferLocation,
+  OfferText,
+  OfferTitle,
+} from "./JobOffers.Style";
 
-interface CompanyProps {
-  company: Company;
-}
-
-const CompanyContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  text-align: left;
-  padding-bottom: 15rem;
-  margin: 1rem 2rem;
-  width: 90%;
-
-  @media (min-width: 768px) {
-    flex-direction: row;
-    justify-content: space-between;
-    align-items: flex-start;
-  }
-
-  svg {
-    max-width: 18px;
-    max-height: 18px;
-  }
-`;
-
-const CompanyLogo = styled.img`
-  width: 50%;
-  max-width: 10rem;
-  margin-bottom: 1rem;
-  text-align: center;
-
-  @media (min-width: 768px) {
-    max-width: none;
-    margin-bottom: 0;
-    margin-right: 1rem;
-  }
-`;
-
-const CompanyName = styled.h2`
-  font-size: 1.5rem;
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-`;
-
-const CompanyDescription = styled.p`
-   {
-    margin: 0.8rem 0;
-  }
-`;
-
-const OfferTitle = styled.h3`
-  font-size: 1.25rem;
-  font-weight: bold;
-  margin-bottom: 0.5rem;
-`;
-
-const OfferText = styled.p`
-  margin-bottom: 1rem;
-  text-align: left;
-
-  ul {
-    margin-bottom: 1rem;
-    margin-left: 2rem;
-  }
-`;
-
-const OfferLocation = styled.p`
-  margin-bottom: 1rem;
-`;
-
-const OfferLink = styled.a`
-  color: ${Color.MAGENTA};
-  font-weight: bold;
-`;
-
-const CompanyOffers: React.FC<CompanyProps> = ({ company }) => {
-  return (
-    <CompanyContainer id={company.name.toLowerCase()}>
-      <div>
-        <a
-          style={{ textAlign: "center" }}
-          target="_blank"
-          rel="noreferrer noopener"
-          href={company.url}
-        >
-          <CompanyLogo src={company.logo} alt={`${company.name} logo`} />
-        </a>
-        <CompanyDescription>{company.description}</CompanyDescription>
-        <p>
-          {company.linkedin && (
-            <LinkedinIcon
-              color={Color.DARK_BLUE}
-              linkedinUrl={company.linkedin}
-            />
-          )}
-          {company.twitter && (
-            <TwitterIcon color={Color.DARK_BLUE} twitterUrl={company.twitter} />
-          )}
-        </p>
-      </div>
-      <div>
-        <CompanyName>{company.name}</CompanyName>
-        {company.offers.map((offer) => (
-          <div key={offer.id}>
-            <OfferTitle>{offer.title}</OfferTitle>
-            <OfferLocation>{offer.location}</OfferLocation>
-            <OfferText dangerouslySetInnerHTML={{ __html: offer.text }} />
-            <OfferLink
-              href={offer.url}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn more
-            </OfferLink>
-          </div>
-        ))}
-      </div>
-    </CompanyContainer>
-  );
-};
+const CompanyOffers: React.FC<CompanyProps> = ({ company }) => (
+  <CompanyContainer id={company.name.toLowerCase()}>
+    <div style={{ flex: "0 0 30%" }}>
+      <a
+        style={{ textAlign: "center" }}
+        target="_blank"
+        rel="noreferrer noopener"
+        href={company.url}
+      >
+        <CompanyLogo src={company.logo} alt={`${company.name} logo`} />
+      </a>
+      <CompanyName>{company.name}</CompanyName>
+      <CompanyDescription>{company.description}</CompanyDescription>
+      <p>
+        {company.linkedin && (
+          <LinkedinIcon
+            color={Color.LIGHT_BLUE}
+            linkedinUrl={company.linkedin}
+          />
+        )}
+        {company.twitter && (
+          <TwitterIcon color={Color.LIGHT_BLUE} twitterUrl={company.twitter} />
+        )}
+      </p>
+    </div>
+    <div style={{ flex: "0 0 70%" }}>
+      {company.offers.map((offer) => (
+        <div key={offer.id}>
+          <OfferTitle>{offer.title}</OfferTitle>
+          <OfferLocation>{offer.location}</OfferLocation>
+          <OfferText dangerouslySetInnerHTML={{ __html: offer.text }} />
+          <OfferLink href={offer.url} target="_blank" rel="noopener noreferrer">
+            Learn more
+          </OfferLink>
+        </div>
+      ))}
+    </div>
+  </CompanyContainer>
+);
 
 export default CompanyOffers;
--- src/views/JobOffers/JobOffers.Style.ts
+++ src/views/JobOffers/JobOffers.Style.ts
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { BIG_BREAKPOINT, TABLET_BREAKPOINT } from "../../constants/BreakPoints";
+import { Color } from "../../styles/colors";
 
 export const StyledWrapperSection = styled.section`
   align-items: center;
@@ -34,4 +35,75 @@ export const StyledMoreIcon = styled.img`
     height: 10rem;
     top: 0;
   }
+`;
+export const CompanyContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: left;
+  padding-bottom: 15rem;
+  margin: 1rem 2rem;
+  width: 90%;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: flex-start;
+  }
+
+  svg {
+    max-width: 18px;
+    max-height: 18px;
+  }
+`;
+export const CompanyLogo = styled.img`
+  width: 50%;
+  max-width: 10rem;
+  margin-bottom: 1rem;
+  text-align: center;
+
+  @media (min-width: 768px) {
+    max-width: none;
+    margin-bottom: 0;
+    margin-right: 1rem;
+  }
+`;
+export const CompanyName = styled.h2`
+  font-size: 1.5rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+`;
+export const CompanyDescription = styled.p`
+  margin: 0.8rem 0;
+  text-align: left;
+  color: ${Color.DARK_BLUE};
+`;
+export const OfferTitle = styled.h3`
+  font-size: 1.25rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  color: ${Color.DARK_GREEN};
+`;
+export const OfferText = styled.p`
+  margin-bottom: 1rem;
+  color: ${Color.DARK_BLUE};
+  text-align: left;
+
+  p {
+    margin: initial;
+    padding: 0.2rem;
+  }
+
+  ul {
+    margin-bottom: 1rem;
+    margin-left: 2rem;
+  }
+`;
+export const OfferLocation = styled.p`
+  margin-bottom: 1rem;
+  color: ${Color.GREEN};
+`;
+export const OfferLink = styled.a`
+  color: ${Color.MAGENTA};
+  font-weight: bold;
 `;
