c7beb4c71c498f1fc4d8852090591590ec6f715e
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-06-04 08:11:49 +0200

    test: diversity tests

--- src/views/Diversity/Diversity.test.tsx
+++ src/views/Diversity/Diversity.test.tsx
@@ -1,16 +1,35 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import Diversity from "./Diversity";
+import { createMemoryHistory } from "history";
+import { ROUTE_HOME } from "../../constants/routes";
+import { Router } from "react-router-dom";
 
 describe("Diversity component", () => {
   it("renders heading correctly", () => {
-    render(<Diversity />);
+    const history = createMemoryHistory();
+    history.push(ROUTE_HOME);
+    render(
+      <React.Suspense>
+        <Router history={history}>
+          <Diversity />
+        </Router>
+      </React.Suspense>
+    );
     const headingElement = screen.getByText("Diversity Sponsorship");
     expect(headingElement).toBeInTheDocument();
   });
 
   it("renders paragraph text correctly", () => {
-    render(<Diversity />);
+    const history = createMemoryHistory();
+    history.push(ROUTE_HOME);
+    render(
+      <React.Suspense>
+        <Router history={history}>
+          <Diversity />
+        </Router>
+      </React.Suspense>
+    );
     const paragraphElement = screen.getByText(
       /DevBcn, its volunteers, and staff consider that understanding/i
     );
@@ -18,7 +37,15 @@ describe("Diversity component", () => {
   });
 
   it("renders sponsor logos correctly", () => {
-    render(<Diversity />);
+    const history = createMemoryHistory();
+    history.push(ROUTE_HOME);
+    render(
+      <React.Suspense>
+        <Router history={history}>
+          <Diversity />
+        </Router>
+      </React.Suspense>
+    );
     const vepeeLogo = screen.getByAltText("Vepee");
     const adevintaLogo = screen.getByAltText("Adevinta");
 
