75a3f0695ef4937eaf216c79f953532622c1df45
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-06-20 22:46:17 +0200

    feat: supporters category

---
+++ src/views/Home/components/Sponsors/Supporters.tsx
@@ -0,0 +1,106 @@
+import {
+  StyledFlexGrow,
+  StyledLogos,
+  StyledSeparator,
+  StyledSlashes,
+  StyledSponsorIconMicro,
+  StyledSponsorItemContainer,
+  StyledSponsorLogosContainer,
+  StyledSponsorTitleContainer,
+  StyledSponsorTitleMargin,
+  StyledSponsorTitleSlashesContainer,
+} from "./Sponsors.style";
+import SponsorBadge from "./SponsorBadge";
+import { Color } from "../../../../styles/colors";
+import { BIG_BREAKPOINT } from "../../../../constants/BreakPoints";
+import { buildSlashes } from "./Sponsors";
+import { useWindowSize } from "react-use";
+import { useCallback, useEffect, useState } from "react";
+import { sponsors } from "./SponsorsData";
+
+export const Supporters = () => {
+  const { width } = useWindowSize();
+  const [slashes, setSlashes] = useState("");
+  const [isHovered, setIsHovered] = useState<boolean>(false);
+  const supporters = sponsors.supporters;
+
+  useEffect(() => {
+    const newSlashes = buildSlashes(2);
+
+    setSlashes(newSlashes);
+  }, [width]);
+
+  const handleHover = useCallback(() => setIsHovered(true), []);
+  const handleUnHover = useCallback(() => setIsHovered(false), []);
+  return (
+    <>
+      {supporters !== null && supporters.length > 0 && (
+        <StyledSponsorItemContainer
+          className="SponsorItem supporters"
+          id="supporters"
+          data-testid="supporters"
+          onMouseEnter={handleHover}
+          onMouseLeave={handleUnHover}
+        >
+          <SponsorBadge
+            color={Color.DARK_BLUE}
+            position="right"
+            isVisible={isHovered}
+          />
+          <StyledSponsorTitleContainer className="SponsorTitle">
+            <StyledSponsorTitleSlashesContainer
+              color={
+                isHovered && width >= BIG_BREAKPOINT
+                  ? Color.WHITE
+                  : Color.DARK_BLUE
+              }
+              id="Slashes"
+            >
+              <StyledSlashes>{slashes}</StyledSlashes>
+              <StyledSeparator />
+
+              {width < BIG_BREAKPOINT && "SUPPORTERS"}
+            </StyledSponsorTitleSlashesContainer>
+            {width >= BIG_BREAKPOINT && (
+              <StyledSponsorTitleSlashesContainer
+                color={
+                  isHovered && width >= BIG_BREAKPOINT
+                    ? Color.WHITE
+                    : Color.DARK_BLUE
+                }
+              >
+                <StyledSlashes>{slashes}</StyledSlashes>
+                SUPPORTERS
+              </StyledSponsorTitleSlashesContainer>
+            )}
+            <StyledSponsorTitleMargin />
+          </StyledSponsorTitleContainer>
+
+          <StyledSponsorLogosContainer className="SponsorLogos">
+            <StyledFlexGrow />
+            <StyledLogos position="right">
+              {supporters.map((sponsor) => (
+                <a
+                  key={sponsor.name}
+                  href={sponsor.website}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <StyledSponsorIconMicro
+                    src={sponsor.image}
+                    alt={sponsor.name}
+                    style={{
+                      filter: isHovered
+                        ? `drop-shadow(2px 2px 2px ${Color.LIGHT_BLUE})`
+                        : `none`,
+                    }}
+                  />
+                </a>
+              ))}
+            </StyledLogos>
+          </StyledSponsorLogosContainer>
+        </StyledSponsorItemContainer>
+      )}
+    </>
+  );
+};
