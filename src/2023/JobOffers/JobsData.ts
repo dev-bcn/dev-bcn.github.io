975b646dbad9af1ff72f85680d9c5aff24d2e4a0
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-12-09 22:01:18 +0100

    chore: remove duplication

--- src/2023/JobOffers/JobsData.ts
+++ src/2023/JobOffers/JobsData.ts
@@ -1,21 +1,4 @@
-export interface Company {
-  id: string;
-  name: string;
-  url: string;
-  logo: string;
-  description: string;
-  twitter?: string;
-  linkedin?: string;
-  offers: Offer[];
-}
-
-export interface Offer {
-  id: string;
-  title: string;
-  location: string;
-  text: string;
-  url: string;
-}
+import { Company } from "../../views/JobOffers/JobsData";
 
 export const jobOffers: Company[] = [
   {
