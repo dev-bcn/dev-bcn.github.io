752c0667b5f89d56b735186d1246ac79d4c181d8
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2023-12-28 13:14:36 +0100

    fix: modify home button behavior

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -19,17 +19,19 @@ const doNothingHandler = (
 };
 
 const StyledActionButton = styled.div`
-  background-color: ${Color.LIGHT_BLUE};
-  text-align: center;
-  font-size: 1.3em;
-  min-width: 200px;
-  margin: 20px 5px;
-  border-radius: 5px;
-  box-shadow: 1px 1px 1px #000;
-  padding: 10px 15px;
-  transform: perspective(1px) translateZ(0);
-  transition-duration: 0.5s;
-  vertical-align: middle;
+   {
+    background-color: ${Color.LIGHT_BLUE};
+    text-align: center;
+    font-size: 1.3em;
+    min-width: 200px;
+    margin: 20px 5px;
+    border-radius: 5px;
+    box-shadow: 1px 1px 1px #000;
+    padding: 10px 15px;
+    transform: perspective(1px) translateZ(0);
+    transition-duration: 0.5s;
+    vertical-align: middle;
+  }
 
   @media (max-width: ${BIG_BREAKPOINT}px) {
     margin: 5px 1px;
@@ -51,11 +53,21 @@ const StyledActionButton = styled.div`
     vertical-align: middle;
   }
 
+  a:hover {
+    background-color: transparent;
+  }
+
   small {
     font-weight: bold;
     font-size: 0.7em;
     color: ${Color.MAGENTA};
     text-shadow: none;
+    transition: color ease-in-out 0.5s;
+  }
+
+  small:hover {
+    color: ${Color.WHITE};
+    background-color: transparent;
   }
 `;
 
