e1362142b4e8902b11de51edf1782d7021585b48
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-06-04 15:10:08 +0200

    feat: live schedule view

--- src/App.tsx
+++ src/App.tsx
@@ -76,6 +76,7 @@ import Schedule from "./views/Schedule/Schedule";
 import Diversity from "./views/Diversity/Diversity";
 import { LiveView } from "./views/Talks/LiveView";
 import Diversity from "./views/Diversity/Diversity";
+import { LiveView } from "./views/Talks/LiveView";
 
 const StyledAppWrapper = styled.div`
   position: relative;
