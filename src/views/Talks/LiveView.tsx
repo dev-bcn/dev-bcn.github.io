e584a0f47d9a948fd962aeb6baa9f7c8f29cde61
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-06-06 18:53:28 +0200

    feat: separate into different components

--- src/views/Talks/LiveView.tsx
+++ src/views/Talks/LiveView.tsx
@@ -1,74 +1,30 @@
 import React, { FC } from "react";
 import { useFetchLiveView } from "./UseFetchTalks";
 import Loading from "../../components/Loading/Loading";
-import moment from "moment";
 import { UngroupedSession } from "./liveView.types";
 import conference from "../../data/2024.json";
-import { TalkCard, TalkCardProps } from "./components/TalkCard";
-import { QuestionAnswers, SessionCategory, SessionSpeaker } from "./Talk.types";
-import styled from "styled-components";
-import {
-  MOBILE_BREAKPOINT,
-  TABLET_BREAKPOINT,
-} from "../../constants/BreakPoints";
+import { TalkCard } from "./components/TalkCard";
 import * as Sentry from "@sentry/react";
-
-export const talkCardAdapter = (
-  session: UngroupedSession,
-  index: number,
-): TalkCardProps => {
-  return {
-    index: index,
-    key: parseInt(session.id),
-    talk: {
-      id: parseInt(session.id),
-      title: session.title,
-      talkImage: 1,
-      speakers: session.speakers.map((speaker) => ({
-        id: speaker.id,
-        name: speaker.name,
-      })) as SessionSpeaker[],
-      level: "Beginner",
-      link: "",
-      tags: [],
-      track: session.room,
-      categories: session.categories.map((category) => ({
-        id: category.id,
-        name: category.name,
-        categoryItems: category.categoryItems,
-      })) as SessionCategory[],
-      questionAnswers: session.questionAnswers.map((qa) => ({
-        id: qa.id,
-        question: qa.question,
-        answer: qa.answer,
-      })) as QuestionAnswers[],
-    },
-    showTrack: true, // Default value, adjust as necessary
-  };
-};
-
-const StyledMain = styled.main`
-  padding-left: 2rem;
-  padding-top: 4rem;
-  padding-bottom: 7rem;
-
-  @media screen and (max-width: ${MOBILE_BREAKPOINT}px) {
-    padding-bottom: 40rem;
-    padding-left: 1rem;
-  }
-
-  @media screen and (min-width: ${TABLET_BREAKPOINT}px) {
-    padding-bottom: 20rem;
-    padding-left: 1rem;
-  }
-`;
+import { StyledMain } from "./Talks.style";
+import { talkCardAdapter } from "./TalkCardAdapter";
 
 export const LiveView: FC<React.PropsWithChildren<unknown>> = () => {
   const { isLoading, error, data } = useFetchLiveView();
 
-  const [today] = React.useState(new Date(2024, 5, 13, 12, 40));
-  const isBetween = (talk: UngroupedSession): boolean =>
-    today >= new Date(talk.startsAt) && today <= new Date(talk.endsAt);
+  const [today] = React.useState(new Date());
+
+  const isBetween = (
+    today: Date,
+    startDate: string,
+    endDate: string,
+  ): boolean => today >= new Date(startDate) && today <= new Date(endDate);
+
+  const getPredicate = () => (session: UngroupedSession) =>
+    isBetween(today, session.startsAt, session.endsAt);
+
+  const [filteredTalks, setFilteredTalks] = React.useState<
+    UngroupedSession[] | undefined
+  >(data?.sessions.filter(getPredicate()));
 
   if (error) {
     Sentry.captureException(error);
@@ -76,7 +32,10 @@ export const LiveView: FC<React.PropsWithChildren<unknown>> = () => {
 
   React.useEffect(() => {
     document.title = `Live view - ${conference.title} - ${conference.edition} Edition`;
-  }, []);
+    if (data) {
+      setFilteredTalks(data?.sessions.filter(getPredicate()));
+    }
+  }, [data, today, getPredicate]);
 
   return (
     <StyledMain>
@@ -90,8 +49,11 @@ export const LiveView: FC<React.PropsWithChildren<unknown>> = () => {
       </h1>
 
       {isLoading && <Loading />}
-      <article>Now: {moment(today).format("LLL")}</article>
-      {data?.sessions.filter(isBetween).map((session, index) => {
+      <article>Live Schedule</article>
+      {!isBetween(today, conference.startDay, conference.endDay) && (
+        <h4>The live schedule is not ready yet </h4>
+      )}
+      {filteredTalks?.map((session, index) => {
         return <TalkCard {...talkCardAdapter(session, index)} />;
       })}
     </StyledMain>
