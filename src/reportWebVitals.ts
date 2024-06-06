4a052c6e4adf9d5d3319324e298270b4bd4ec6eb
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-06-07 00:15:31 +0200

    ci: update node version

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,13 +1,13 @@
-import { ReportCallback } from "web-vitals";
+import { onCLS, onFCP, onFID, onLCP, onTTFB } from "web-vitals";
 
-const reportWebVitals = (onPerfEntry?: ReportCallback) => {
+const reportWebVitals = (onPerfEntry?: any) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import("web-vitals").then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+    import("web-vitals").then(() => {
+      onCLS(onPerfEntry);
+      onFID(onPerfEntry);
+      onFCP(onPerfEntry);
+      onLCP(onPerfEntry);
+      onTTFB(onPerfEntry);
     });
   }
 };
