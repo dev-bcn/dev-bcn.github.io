a1f8ad4cafbd2d202d92e73f31f5aafe0fba5716
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-06-06 17:56:05 +0200

    feat: separate into different components

---
+++ src/views/Talks/TalkCardAdapter.ts
@@ -0,0 +1,37 @@
+import { UngroupedSession } from "./liveView.types";
+import { TalkCardProps } from "./components/TalkCard";
+import { QuestionAnswers, SessionCategory, SessionSpeaker } from "./Talk.types";
+
+export const talkCardAdapter = (
+  session: UngroupedSession,
+  index: number,
+): TalkCardProps => {
+  return {
+    index: index,
+    key: parseInt(session.id),
+    talk: {
+      id: parseInt(session.id),
+      title: session.title,
+      talkImage: 1,
+      speakers: session.speakers.map((speaker) => ({
+        id: speaker.id,
+        name: speaker.name,
+      })) as SessionSpeaker[],
+      level: "Beginner",
+      link: "",
+      tags: [],
+      track: session.room,
+      categories: session.categories.map((category) => ({
+        id: category.id,
+        name: category.name,
+        categoryItems: category.categoryItems,
+      })) as SessionCategory[],
+      questionAnswers: session.questionAnswers.map((qa) => ({
+        id: qa.id,
+        question: qa.question,
+        answer: qa.answer,
+      })) as QuestionAnswers[],
+    },
+    showTrack: true, // Default value, adjust as necessary
+  };
+};
