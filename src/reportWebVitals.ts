3a4ca3d7d7425edb0f5632cd220eb433998c5a4a
Anyul Rivas <Anyul.Rivas@ingrammicro.com>
2024-06-09 17:52:42 +0200

    fix: fox sonar error

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,10 +1,9 @@
-import { onCLS, onFCP, onFID, onLCP, onTTFB } from "web-vitals";
+import { onCLS, onFCP, onLCP, onTTFB } from "web-vitals";
 
 const reportWebVitals = (onPerfEntry?: any) => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
     import("web-vitals").then(() => {
       onCLS(onPerfEntry);
-      onFID(onPerfEntry);
       onFCP(onPerfEntry);
       onLCP(onPerfEntry);
       onTTFB(onPerfEntry);
