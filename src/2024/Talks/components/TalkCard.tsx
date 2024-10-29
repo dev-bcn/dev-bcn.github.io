97e0340a0164b02c4f8039e71b44299a7d5b64b2
Anyul Rivas <anyul.rivas@ingrammicro.com>
2024-10-29 19:33:18 +0100

    feat: talks 2024

---
+++ src/2024/Talks/components/TalkCard.tsx
@@ -0,0 +1,101 @@
+import React, {FC} from "react";
+import {Link} from "react-router-dom";
+import {Tag} from "../../../components/Tag/Tag";
+import {
+  ROUTE_2024_SPEAKER_DETAIL,
+  ROUTE_2024_TALK_DETAIL,
+} from "../../../constants/routes";
+
+import {
+  extractSessionCategoryInfo,
+  extractSessionTags,
+} from "../UseFetchTalks";
+
+import {Color} from "../../../styles/colors";
+import {StyledJobsInfo} from "../../../views/JobOffers/components/JobsCard";
+import {
+  CategoryItemEnum,
+  QuestionAnswers,
+  SessionCategory,
+  SessionSpeaker
+} from "../../../views/Talks/Talk.types";
+import {
+  StyledSessionCard,
+  StyledSessionText,
+  StyledTagsWrapper,
+  StyledTalkSpeaker,
+  StyledTalkTitle
+} from "../../../views/Talks/Talks.style";
+import {StyledVoteTalkLink} from "../../../views/MeetingDetail/MeetingDetail";
+
+export interface TalkCardProps {
+    talk: {
+        id: number;
+        title: string;
+        talkImage?: number;
+        speakers: SessionSpeaker[];
+        level?: string;
+        link?: string;
+        tags?: string[];
+        track: string;
+        categories: SessionCategory[];
+        questionAnswers: QuestionAnswers[];
+    };
+    showTrack?: boolean;
+}
+
+export const TalkCard: FC<React.PropsWithChildren<TalkCardProps>> = ({
+                                                                         showTrack = false,
+                                                                         talk,
+                                                                     }) => {
+    return (
+        <StyledSessionCard>
+            <StyledJobsInfo>
+                <StyledTalkTitle to={`${ROUTE_2024_TALK_DETAIL}/${talk.id}`}>
+                    {talk.title}
+                </StyledTalkTitle>
+                <StyledSessionText>
+                    {talk.speakers.map((speaker: SessionSpeaker) => (
+                        <StyledTalkSpeaker key={speaker.id}>
+                            <Link
+                                to={`${ROUTE_2024_SPEAKER_DETAIL}/${speaker.id}`}>
+                                {speaker.name}
+                            </Link>
+                        </StyledTalkSpeaker>
+                    ))}
+                </StyledSessionText>
+                <StyledSessionText>
+                    {`${extractSessionCategoryInfo(
+                        talk.categories,
+                        CategoryItemEnum.Format,
+                    )} `}
+                    {extractSessionCategoryInfo(talk.categories)}{" "}
+                </StyledSessionText>
+                {showTrack && (
+                    <StyledSessionText>
+                        <strong>Track: </strong>
+                        {extractSessionCategoryInfo(
+                            talk.categories,
+                            CategoryItemEnum.Track,
+                        )}
+                    </StyledSessionText>
+                )}
+                <StyledTagsWrapper>
+                    {extractSessionTags(talk.questionAnswers)?.map((tag) => {
+                        return <Tag key={tag} text={tag}
+                                    textColor={Color.WHITE}/>;
+                    })}
+                </StyledTagsWrapper>
+                <section>
+                    <StyledVoteTalkLink
+                        href={`https://openfeedback.io/devbcn24/0/${talk.id}`}
+                        target={"_blank"}
+                        rel="noreferrer noopener"
+                    >
+                        🗳️ <strong>Vote this talk</strong>
+                    </StyledVoteTalkLink>
+                </section>
+            </StyledJobsInfo>
+        </StyledSessionCard>
+    );
+};
