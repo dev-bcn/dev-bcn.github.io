8f83f17e2ba1f0f8c8a1771d41aefdbece989fff
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-02-23 15:05:13 +0100

    feat: replace sessionize API url for 2025

--- src/views/Speakers/UseFetchSpeakers.ts
+++ src/views/Speakers/UseFetchSpeakers.ts
@@ -6,7 +6,7 @@ import {speakerAdapter} from "../../services/speakerAdapter";
 export const useFetchSpeakers = (id?: string): UseQueryResult<ISpeaker[]> => {
   return useQuery("api-speakers", async () => {
     const serverResponse = await axios.get(
-      "https://sessionize.com/api/v2/teq4asez/view/Speakers",
+      "https://sessionize.com/api/v2/xhudniix/view/Speakers",
     );
     let returnData;
     if (id !== undefined) {
