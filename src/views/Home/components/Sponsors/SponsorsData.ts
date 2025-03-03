b5618691205d97a55bd55b8c24b57d3fb452e154
Anyul Rivas <anyulled@gmail.com>
2025-03-03 08:38:46 +0100

    feat: caixabank as regular sponsor for 2025 edition (#659)

--- src/views/Home/components/Sponsors/SponsorsData.ts
+++ src/views/Home/components/Sponsors/SponsorsData.ts
@@ -1,25 +1,29 @@
 export interface Sponsors {
-  top: Sponsor[] | null;
-  premium: Sponsor[] | null;
-  regular: Sponsor[] | null;
-  communities: Sponsor[] | null;
-  basic: Sponsor[] | null;
-  media_partners: Sponsor[] | null;
-  supporters: Sponsor[] | null;
+    top: Sponsor[] | null;
+    premium: Sponsor[] | null;
+    regular: Sponsor[] | null;
+    communities: Sponsor[] | null;
+    basic: Sponsor[] | null;
+    media_partners: Sponsor[] | null;
+    supporters: Sponsor[] | null;
 }
 
 export interface Sponsor {
-  name: string;
-  website: string;
-  image: string;
+    name: string;
+    website: string;
+    image: string;
 }
 
 export const sponsors: Sponsors = {
-  top: [],
-  premium: [],
-  regular: [],
-  basic: [],
-  communities: [],
-  media_partners: [],
-  supporters: [],
+    top: [],
+    premium: [],
+    regular: [{
+        name: "Caixabank Tech",
+        website: "https://www.caixabanktech.com/es/pagina-de-inicio/",
+        image: "images/sponsors/caixabank-tech.png",
+    }],
+    basic: [],
+    communities: [],
+    media_partners: [],
+    supporters: [],
 };
