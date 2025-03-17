6e091274389b489f0fd74e599846259b2b7e6bc7
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-03-17 20:40:38 +0100

    feat: remove hardcoded speaker

--- src/views/Speakers/UseFetchSpeakers.ts
+++ src/views/Speakers/UseFetchSpeakers.ts
@@ -1,7 +1,7 @@
-import {useQuery, UseQueryResult} from "react-query";
+import { useQuery, UseQueryResult } from "react-query";
 import axios from "axios";
-import {speakerAdapter} from "../../services/speakerAdapter";
-import {IResponse, ISpeaker} from "../../types/speakers";
+import { speakerAdapter } from "../../services/speakerAdapter";
+import { ISpeaker } from "../../types/speakers";
 
 export const useFetchSpeakers = (id?: string): UseQueryResult<ISpeaker[]> => {
   return useQuery("api-speakers", async () => {
@@ -17,25 +17,6 @@ export const useFetchSpeakers = (id?: string): UseQueryResult<ISpeaker[]> => {
       returnData = serverResponse.data;
     }
 
-    const victorRentea: IResponse = {
-      sessions: [],
-      id: "cccf2456-cddf-4121-a668-d5f9f509f82e",
-      bio: "With two decades of experience, Victor is a Java Champion working as a trainer for top companies in Europe. More than five thousand developers of 120 companies attended his workshops, so every week he has the opportunity to debate with bright engineers the challenges faced by their projects. In return, Victor summarizes key learning points from these workshops in conference talks and online meetups for the European Software Crafters, the world's largest community around architecture, refactoring, and testing. Find out how Victor can help you on https://victorrentea.ro: training catalog, consultancy, and YouTube playlists of his talks.",
-      fullName: "Victor Rentea",
-      links: [{
-        url: "https://x.com/victorrentea",
-        linkType: "LinkedIn",
-        title: "LinkedIn"
-      }, {
-        url: "https://www.linkedin.com/in/victor-rentea-trainer",
-        linkType: "Twitter",
-        title: "Twitter"
-      }],
-      tagLine: "Java Champion and Trainer",
-      profilePicture: "https://sessionize.com/image/3031-0o0o0-a3r6JkTgm9aUHJXBhbvnWQ.jpg?download=victor-rentea.jpg",
-    }
-
-    return speakerAdapter([...returnData, victorRentea]);
+    return speakerAdapter(returnData);
   });
 };
-
