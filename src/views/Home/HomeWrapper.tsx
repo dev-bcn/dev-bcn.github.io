33d5559cf523cc9d92e97ca48b86c7c9fab9266c
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-03-28 13:30:49 +0100

    fix: fix 2025 edition speaker link

--- src/views/Home/HomeWrapper.tsx
+++ src/views/Home/HomeWrapper.tsx
@@ -1,52 +1,49 @@
-import { BIG_BREAKPOINT } from "../../constants/BreakPoints";
-import React, { FC } from "react";
+import {BIG_BREAKPOINT} from "../../constants/BreakPoints";
+import React, {FC} from "react";
 import Faqs from "./components/Faqs/Faqs";
 import Home from "./components/Home/Home";
 import Sponsors from "./components/Sponsors/Sponsors";
 import styled from "styled-components";
 import conferenceData from "../../data/2025.json";
-import { useLocation } from "react-router";
+import {useLocation} from "react-router";
 
 import SpeakersCarousel from "../../components/Swiper/SpeakersCarousel";
-import { ROUTE_SPEAKERS } from "../../constants/routes";
-import { ErrorBoundary } from "react-error-boundary";
-import { useDocumentTitleUpdater } from "../../hooks/useDocumentTitleUpdate";
+import {ROUTE_SPEAKERS} from "../../constants/routes";
+import {useDocumentTitleUpdater} from "../../hooks/useDocumentTitleUpdate";
 
 const StyledContainer = styled.div`
-  padding-bottom: 10rem;
+    padding-bottom: 10rem;
 
-  @media only screen and (max-width: ${BIG_BREAKPOINT}px) {
-    padding-bottom: 20rem;
-  }
+    @media only screen and (max-width: ${BIG_BREAKPOINT}px) {
+        padding-bottom: 20rem;
+    }
 `;
 
 const HomeWrapper: FC<React.PropsWithChildren<unknown>> = () => {
-  const { hash } = useLocation();
+    const {hash} = useLocation();
 
-  React.useEffect(() => {
-    if (hash != null && hash !== "") {
-      const scroll = document.getElementById(hash.substring(1));
-      scroll?.scrollIntoView();
-    }
-  }, [hash]);
+    React.useEffect(() => {
+        if (hash != null && hash !== "") {
+            const scroll = document.getElementById(hash.substring(1));
+            scroll?.scrollIntoView();
+        }
+    }, [hash]);
 
-  useDocumentTitleUpdater("Home", conferenceData?.edition ?? "2025");
+    useDocumentTitleUpdater("Home", conferenceData?.edition ?? "2025");
 
-  return (
-    <StyledContainer id="home-wrapper">
-      <ErrorBoundary fallback={<div>Something went wrong</div>}>
-        <Home />
-      </ErrorBoundary>
-      <Faqs />
-      {conferenceData?.carrousel.enabled && (
-        <SpeakersCarousel
-          sessionizeUrl={conferenceData.sessionizeUrl}
-          speakersLink={ROUTE_SPEAKERS}
-        />
-      )}
-      <Sponsors />
-    </StyledContainer>
-  );
+    return (
+        <StyledContainer id="home-wrapper">
+            <Home/>
+            <Faqs/>
+            {conferenceData?.carrousel.enabled && (
+                <SpeakersCarousel
+                    sessionizeUrl={conferenceData.sessionizeUrl}
+                    speakersLink={ROUTE_SPEAKERS}
+                />
+            )}
+            <Sponsors/>
+        </StyledContainer>
+    );
 };
 
 export default HomeWrapper;
