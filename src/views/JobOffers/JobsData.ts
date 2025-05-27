60e7d61cca2ab114297727888e885623ed02bffe
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-05-27 15:25:49 +0200

    feat: update job offers data and add Manychat listings

--- src/views/JobOffers/JobsData.ts
+++ src/views/JobOffers/JobsData.ts
@@ -17,8 +17,8 @@ export const jobOffers: Company[] = [
         url: "https://caixabanktech.com/es/job/software-engineer-javapython/", // Removed newline
         title: "Software Engineer Java+Python",
         text:
-          "Programación y desarrollo de productos, proyectos y soluciones en el sector financiero\n" +
-          "Trabajo en modo Lab, con equipo multidisciplinar en metodología Agile\n" +
+          "Programación y desarrollo de productos, proyectos y soluciones en el sector financiero<br/>" +
+          "Trabajo en modo Lab, con equipo multidisciplinar en metodología Agile<br/>" +
           "Desarrollo integral de software comprendiendo todo el ciclo de vida: revisión de requerimientos, análisis de la solución, programación y pruebas, revisión del incidental y evolución",
         location: "Hybrid",
       },
@@ -26,20 +26,20 @@ export const jobOffers: Company[] = [
   },
   {
     id: "cec41b8a-e8d9-447e-83e7-099680c76181",
-    url: "https://www.snowflake.com/en/uk/\n",
+    url: "https://www.snowflake.com/en/uk",
     name: "Snowflake",
     logo: "/images/sponsors/logo-snowflake.png",
     description:
-      "Snowflake makes enterprise AI easy, efficient and trusted. More than 11,000 companies around the globe, including hundreds of the world’s largest, use Snowflake’s AI Data Cloud to share data, build applications, and power their business with AI. The era of enterprise AI is here. \n",
+      "Snowflake makes enterprise AI easy, efficient and trusted. More than 11,000 companies around the globe, including hundreds of the world’s largest, use Snowflake’s AI Data Cloud to share data, build applications, and power their business with AI. The era of enterprise AI is here. <br/>",
     twitter: "https://twitter.com/SnowflakeDB",
     linkedin: "https://www.linkedin.com/company/snowflake-computing",
     offers: [
       {
         id: "c50b1d5f-113a-4ef7-8dc5-20717ac93bc0",
-        text: "We’re searching for a talented Senior (Staff) Software Engineer to join us for our efforts building out the Snowflake platform with cognitive capabilities. Our team at Snowflake has responsibility for moving boundaries of NLP space to SOTA levels and coming up with new solutions in cognitive space. As our team is growing rapidly to keep up with this key responsibility alongside Snowflake’s immense growth, we need additional talent to help lead the change.\n",
+        text: "We’re searching for a talented Senior (Staff) Software Engineer to join us for our efforts building out the Snowflake platform with cognitive capabilities. Our team at Snowflake has responsibility for moving boundaries of NLP space to SOTA levels and coming up with new solutions in cognitive space. As our team is growing rapidly to keep up with this key responsibility alongside Snowflake’s immense growth, we need additional talent to help lead the change.<br/>",
         title: "Senior Software Engineer for AI Poland",
         location: "Hybrid",
-        url: "https://careers.snowflake.com/us/en/job/SNCOUSDAD8F5157E7341298158AD41DE813A7CEXTERNALENUS768DA4518F124A419487D786ECFC578C/Senior-Software-Engineer-for-AI-Poland\n",
+        url: "https://careers.snowflake.com/us/en/job/SNCOUSDAD8F5157E7341298158AD41DE813A7CEXTERNALENUS768DA4518F124A419487D786ECFC578C/Senior-Software-Engineer-for-AI-Poland",
       },
       {
         id: "93c2f7fd-8718-4368-983e-2e8d28fdf06c",
@@ -59,8 +59,32 @@ export const jobOffers: Company[] = [
         id: "d17366a8-6ab1-4908-aa8c-1ccc3897f97c",
         title: "Engineering Manager - Query Execution Platform",
         location: "Hybrid",
-        text: "We’re looking for an exceptional and experienced Engineering Manager to manage and grow the Snowflake query processing team in Berlin, Germany. The team is critical to Snowflake's success, responsible for building new SQL features and customer-facing functionality, developing novel query optimization and query execution techniques for industry-leading performance, and building a database system that's highly parallel, efficient and fault-tolerant.\n",
+        text: "We’re looking for an exceptional and experienced Engineering Manager to manage and grow the Snowflake query processing team in Berlin, Germany. The team is critical to Snowflake's success, responsible for building new SQL features and customer-facing functionality, developing novel query optimization and query execution techniques for industry-leading performance, and building a database system that's highly parallel, efficient and fault-tolerant.<br/>",
         url: "https://careers.snowflake.com/us/en/job/SNCOUS3C3A05E2D0904E3BAC0402E338567595EXTERNALENUS247840B3433245CE9ADE0424A0F27290/Engineering-Manager-Query-Execution-Platform",
+      },
+    ],
+  },
+  {
+    id: "e7bd4ce1-ca17-400f-89de-ebb4a98a5b4d",
+    url: "https://careers.manychat.com",
+    name: "Manychat",
+    logo: "/images/sponsors/logo-manychat.webp",
+    linkedin: "https://www.linkedin.com/company/manychat",
+    twitter: "https://x.com/manychathq",
+    description:
+      "Manychat is a leading Chat Marketing platform. As an official Meta Business Partner, we help businesses engage with their customers on Instagram, TikTok, Facebook Messenger, WhatsApp, and Telegram.",
+    offers: [
+      {
+        id: "7359fdee-d287-4d45-89f5-b251d07f3e83",
+        title: "Senior Backend Engineer (Mobile Team)",
+        text:
+          "We are seeking a Senior PHP Developer who’s all about building smart tech and even better experiences for users. Our cross-functional mobile teams cover a wide range of tasks, allowing  engineers to work on various product components. We believe that mobile is key to our platform’s future, and you’ll play a key role in shaping that future.<br/>" +
+          "<br/>" +
+          "Here, product development goes beyond coding — it’s about creating meaningful value. We’re looking for someone who sees technology as a way to deliver business wins and takes technical quality seriously.<br/>" +
+          "<br/>" +
+          "You’ll join an engineering culture, where ownership is real: teams decide how they build and deliver, always with company goals in mind. Plus, you’ll connect with our internal tech groups, helping raise the bar even higher.",
+        location: "Hybrid",
+        url: "https://grnh.se/883ec4c92us",
       },
     ],
   },
