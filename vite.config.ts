a9e93eaaeda044f5c622e2477c8635143b72d956
Anyul Rivas <anyul.rivas@ingrammicro.com>
2025-06-27 07:29:20 +0200

    chore(deps): update dependencies and add Sentry Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import pkg from "./package.json";
 import path from "path";
 
@@ -10,7 +11,14 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    plugins: [react()],
+    plugins: [
+      react(),
+      sentryVitePlugin({
+        authToken: process.env.SENTRY_AUTH_TOKEN,
+        org: "dev-bcn",
+        project: "devbcn",
+      }),
+    ],
     resolve: {
       alias: {
         // Path aliases matching those in tsconfig.json
@@ -45,6 +53,7 @@ export default defineConfig(({ mode }) => {
     },
     build: {
       outDir: "build", // Match CRA's output directory
+      sourcemap: true,
     },
     server: {
       port: 3000, // Match CRA's default port
