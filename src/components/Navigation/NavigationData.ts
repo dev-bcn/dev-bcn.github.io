2581945d32d9bf3742fcf0523ba39c793bc016a5
Anyul Rivas <anyulled@gmail.com>
2025-12-08 20:48:36 +0100

    ci: update SonarCloud GitHub Action and comment out schedule navigation link.

--- src/components/Navigation/NavigationData.ts
+++ src/components/Navigation/NavigationData.ts
@@ -35,7 +35,7 @@ export const subMenuItems2026: NavigationItem[] = [
   { id: "Home", link: ROUTE_2026_HOME },
   { id: "Code of Conduct", link: ROUTE_2026_CODE_OF_CONDUCT },
   { id: "Sponsors", link: "/2026/#sponsors" },
-  { id: "SCHEDULE", link: ROUTE_2026_SCHEDULE },
+  //{ id: "SCHEDULE", link: ROUTE_2026_SCHEDULE },
   //{ id: "Talks", link: ROUTE_2026_TALKS },
   //{ id: "Workshops", link: ROUTE_2026_WORKSHOPS },
   //{ id: "JOB OFFERS", link: ROUTE_2026_JOB_OFFERS },
