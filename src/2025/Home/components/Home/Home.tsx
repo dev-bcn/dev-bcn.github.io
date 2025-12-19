2d28c68c0bbfb75dab3760661e2a43ea8a4bdd3d
Anyul Rivas <anyulled@gmail.com>
2025-12-19 11:32:56 +0100

    refactor: Extract and centralize countdown components and date utilities into common modules.

--- src/2025/Home/components/Home/Home.tsx
+++ src/2025/Home/components/Home/Home.tsx
@@ -2,7 +2,10 @@ import Countdown from "react-countdown";
 import React, { FC } from "react";
 import { SectionWrapper } from "@components/SectionWrapper/SectionWrapper";
 import { BIGGER_BREAKPOINT } from "@constants/BreakPoints";
-import TimeCountDown from "@views/Home/components/Home/components/TimeCountdown";
+import {
+  TimeCountDown,
+  CountDownCompleted,
+} from "@components/common/countdown";
 import { useWindowSize } from "react-use";
 import { useDateInterval } from "@hooks/useDateInterval";
 // @ts-expect-error some quirky import
@@ -24,10 +27,9 @@ import {
 import ActionButtons from "@views/Home/components/ActionButtons/ActionButtons";
 import { Color } from "@styles/colors";
 import InfoButtons from "../InfoButtons/InfoButtons";
-import { formatDateRange } from "./DateUtil";
+import { formatDateRange } from "@utils/dateUtils";
 import { Link } from "react-router";
 import edition from "@data/2025.json";
-import CountDownCompleted from "@views/Home/components/Home/components/CountDownCompleted";
 
 const Home: FC<React.PropsWithChildren<unknown>> = () => {
   const { width } = useWindowSize();
